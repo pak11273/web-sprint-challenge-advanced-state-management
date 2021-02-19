5f4c023e1b6f8630672b116c08843c1a9f090954
pak11273 <pak11273@gmail.com>
2021-02-19 08:54:50 -0600

    adds actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
+
+// types
+export const FETCH_SMURFS = "FETCH_SMURFS";
+export const ADD_SMURFS = "ADD_SMURFS";
+export const SET_ERROR = "SET_ERROR";
+
+// actions
+export const fetchSmurfs = () => {
+  // isLoading: true
+  return {
+    type: FETCH_SMURFS,
+  };
+};
+
+export const addSmurf = () => {
+  //   [name, nickname, position, summary];
+  return {
+    type: ADD_SMURFS,
+  };
+};
+
+export const setError = () => {
+  return {
+    type: SET_ERROR,
+    error: "This is an error",
+  };
+};
 
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
